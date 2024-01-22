24009a49cb5440944b63710f665007e7d1bfcb0b
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2024-01-22 16:26:08 +0300

    bug_fix

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,6 +1,7 @@
 const {db}=require("../db/db.js")
 const {checkUrl,shortenUrl}=require("../utils/utils.js")
 const server=`http://localhost:5000/api/search?url=`
+let data={}
 function home(req,res){
 return res.render("homePage")
 }
@@ -20,32 +21,48 @@ async function addUrlToDB(req,res)
     const url=req.body.link
     const shortUrl=shortenUserUrl(url)
 try{
-const data={}
-data["url"]=`${server}${shortUrl}`
-    await db.query("insert into url(url,url_short,clicks) values($1,$2,$3)",[url,shortUrl,0])
 
+await db.query("select * from url where url=$1",[url],async (err,result)=>{
+    
+    if(result.rows.length<1)
+    {
+        await db.query("insert into url(url,url_short,clicks) values($1,$2,$3)",[url,shortUrl,0])
+        data["url"]=`${server}${shortUrl}`
+    }
+    else{
+        data["url"]=`${server}${result.rows[0]["url_short"]}`
+    
+    }
     return res.render("url-page.ejs",{ data: data })
+})
+
+   
 }
 catch(err)
 {
     console.log(err)
 }
 }
-function shortUrlPage(req,res)
-{
-    return res.render("url-page.ejs")
-}
+
 async function getUrl(req,res){
 const url=req.query.url
-let searchedPage=undefined
+
 try{
 await db.query("select url from url where url_short=$1",[url],(err,result)=>{
-    const pageUrl=result.rows[0]["url"]
+if(err)
+{
+console.log(err)
+}
+
+    if(result.rows[0])
+    {
+         const pageUrl=result.rows[0]["url"]
     if(pageUrl)
     {
         return res.redirect(pageUrl)
     }
-    else{
+    }
+   else{
         return res.render("homePage.ejs")
     }
 })
@@ -57,4 +74,4 @@ catch(err)
 
 
 }
-module.exports={home,addUrlToDB,shortUrlPage,getUrl}
+module.exports={home,addUrlToDB,getUrl}
