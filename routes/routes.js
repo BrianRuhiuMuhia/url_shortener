afa697e0ef6120febd6f52747a4e613e3317e0b9
brianruhiu <116198613+BrianRuhiuMuhia@users.noreply.github.com>
2024-08-22 13:40:43 +0300

    rewrite

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,5 @@
 const express=require("express")
 const router=express.Router()
-const {home, addUrlToDB,getUrl}=require("../controllers/controllers.js")
-router.get("/home",home)
-router.post("/shorten",addUrlToDB)
-router.get("/search",getUrl)
-
+router.get("/home",homePage)
+router.post("/shorten",)
 module.exports=router
